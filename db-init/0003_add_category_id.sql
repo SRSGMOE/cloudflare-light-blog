d609ba85c9a2a58c173bcb90e35415b71c50fae4
noamoe <92160773+SRSGMOE@users.noreply.github.com>
2026-09-05 23:55:49 +0800

    Delete db-init directory

--- db-init/0003_add_category_id.sql
+++
@@ -1,6 +0,0 @@
--- 添加 category_id 字段到 navs 表
-ALTER TABLE navs ADD COLUMN category_id TEXT DEFAULT '';
-
--- 更新现有数据，将 category 名称转换为 category_id
--- 注意：这需要根据实际数据进行调整
--- UPDATE navs SET category_id = (SELECT id FROM categories WHERE categories.name = navs.category) WHERE category != '';
--- db-init/0004_add_category_id_to_categories.sql
+++
@@ -1,2 +0,0 @@
--- 添加 category_id 字段到 categories 表
-ALTER TABLE categories ADD COLUMN category_id TEXT DEFAULT '';
